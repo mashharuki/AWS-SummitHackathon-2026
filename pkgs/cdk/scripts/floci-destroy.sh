d1d3a98b2cd93d72557dda50df2c813df645cdfe
mashharuki <avp.104.106.107@ae.auone-net.jp>
2026-05-17 20:46:18 +0900

    floci操作用のスクリプトを追加

---
+++ pkgs/cdk/scripts/floci-destroy.sh
@@ -0,0 +1,6 @@
+export AWS_ENDPOINT_URL=http://localhost:4566 
+export AWS_ACCESS_KEY_ID=dummy 
+export AWS_SECRET_ACCESS_KEY=dummy 
+export CDK_DEFAULT_ACCOUNT=000000000000 
+export CDK_DEFAULT_REGION=ap-northeast-1 
+cdk destroy --all --force
